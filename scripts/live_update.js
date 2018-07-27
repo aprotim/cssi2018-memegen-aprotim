c5ea2fdb2d8c5fac5c64422baef25b92c235b010
Aprotim Sanyal <aprotim@aprotim-macbookpro3.roam.corp.google.com>
2018-07-27 09:27:56 -0700

    Added a javascript convenience function

---
+++ scripts/live_update.js
@@ -0,0 +1,10 @@
+function new_element(tag_name, attributes, children=[]){
+  el = document.createElement(tag_name)
+  for (let attr in attributes){
+    el.setAttribute(attr, attributes[attr]);
+  }
+  for (let child in children){
+    el.appendChild(children[child]);
+  }
+  return el
+}
